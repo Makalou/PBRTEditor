49e74155e7b8b0b26d350b496eb3ab3062bc5223
王泽远 <1692167054@qq.com>
2024-02-22 22:54:58 -0700

    some changes

--- src/pbrt_scene_editor/RenderScene.cpp
+++ src/pbrt_scene_editor/RenderScene.cpp
@@ -90,8 +90,12 @@ namespace renderScene
                 for(auto shape : node->shapes)
                 {
                     std::string shape_uuid;
-                    if(typeid(*shape) == typeid(PLYMeshShape)) {
+                    PLYMeshShape * plyMeshPtr = dynamic_cast<PLYMeshShape *>(shape);
+                    if(plyMeshPtr != nullptr)
+                    {
                         shape_uuid = dynamic_cast<PLYMeshShape *>(shape)->filename;
+                    }else{
+                        throw std::runtime_error("Only support ply mesh for now");
                     }
 
                     MeshRigidHandle meshHandle;
@@ -210,103 +214,6 @@ namespace renderScene
             }
             handleNode(node,glm::identity<glm::mat4>());
         }
-
-        /*auto visitor = [this, &assetManager](SceneGraphNode* node)->void{
-            if(!node->shapes.empty())
-            {
-                for(auto shape : node->shapes)
-                {
-                    std::string shape_uuid;
-                    if(typeid(*shape) == typeid(PLYMeshShape)) {
-                        shape_uuid = dynamic_cast<PLYMeshShape *>(shape)->filename;
-                    }
-
-                    MeshRigidHandle meshHandle;
-
-                    for(int i = 0; i < meshes.size(); i ++)
-                    {
-                        if(meshes[i].first == shape_uuid)
-                        {
-                            meshHandle.scene = this;
-                            meshHandle.idx = i;
-                            break;
-                        }
-                    }
-
-                    if(meshHandle.scene == nullptr)
-                    {
-                        // Existing mesh not found
-                        MeshHostObject* meshHost = assetManager.getOrLoadPBRTPLY(shape_uuid);
-
-                        auto interleaveAttribute = meshHost->getInterleavingAttributes();
-
-                        unsigned int * indicies = meshHost->indices.get();
-                        auto indexBufferSize = meshHost->index_count * sizeof(indicies[0]);
-                        auto vertexBufferSize = meshHost->vertex_count * interleaveAttribute.second.VertexStride;
-                        auto interleavingBufferAttribute = interleaveAttribute.second;
-
-                        auto vertexBuffer = backendDevice->allocateBuffer(vertexBufferSize,(VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT),VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE);
-                        auto indexBuffer = backendDevice->allocateBuffer(indexBufferSize,(VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT),VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE);
-
-                        if(!vertexBuffer)
-                        {
-                            throw std::runtime_error("Failed to allocate vertex buffer");
-                        }
-
-                        if(!indexBuffer)
-                        {
-                            throw std::runtime_error("Failed to allocate index buffer");
-                        }
-
-                        MeshRigid::VertexAttribute vertexAttribute;
-                        vertexAttribute.stride = interleavingBufferAttribute.VertexStride;
-                        vertexAttribute.normalOffset = interleavingBufferAttribute.normalOffset;
-                        vertexAttribute.tangentOffset = interleavingBufferAttribute.tangentOffset;
-                        vertexAttribute.biTangentOffset = interleavingBufferAttribute.biTangentOffset;
-                        vertexAttribute.uvOffset = interleavingBufferAttribute.uvOffset;
-
-                        MeshRigid meshRigid{vertexAttribute,static_cast<uint32_t>(meshes.size())};
-
-                        meshRigid.vertexBuffer = vertexBuffer.value();
-                        meshRigid.indexBuffer = indexBuffer.value();
-                        meshRigid.vertexCount = meshHost->vertex_count;
-                        meshRigid.indexCount = meshHost->index_count;
-
-                        backendDevice->oneTimeUploadSync(meshHost->indices.get(),indexBufferSize,meshRigid.indexBuffer.buffer);
-                        backendDevice->oneTimeUploadSync(interleaveAttribute.first,vertexBufferSize,meshRigid.vertexBuffer.buffer);
-
-                        meshes.emplace_back(shape_uuid,meshRigid);
-                        AABB aabb{};
-                        aabb.minX = meshHost->aabb[0]; aabb.minY = meshHost->aabb[1]; aabb.minZ = meshHost->aabb[2];
-                        aabb.maxX = meshHost->aabb[3]; aabb.maxY = meshHost->aabb[4]; aabb.maxZ = meshHost->aabb[5];
-                        aabbs.emplace_back(aabb);
-                        meshHandle.scene = this;
-                        meshHandle.idx = meshes.size() - 1;
-                    }
-
-                    bool foundInstance = false;
-
-                    for(auto & inst : _dynamicRigidMeshBatch)
-                    {
-                        if(inst.mesh == meshHandle)
-                        {
-                            foundInstance = true;
-                            inst.perInstanceData.push_back({node->_finalTransform});
-                            break;
-                        }
-                    }
-
-                    if(!foundInstance)
-                    {
-                        InstanceBatchRigidDynamic<PerInstanceData> meshInstanceRigidDynamic(meshHandle);
-                        meshInstanceRigidDynamic.perInstanceData.push_back({node->_finalTransform});
-                        _dynamicRigidMeshBatch.push_back(meshInstanceRigidDynamic);
-                    }
-                }
-            }
-        };
-
-        root->visit(visitor);*/
 
         /*
          * From a data-oriented view, all entities in renderScene is data, instead of object.
